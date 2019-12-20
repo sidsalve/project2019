26859bfa9911abd5f5b9a42ddadabcf3517c641e
sidsalve <sidsalve1925@gmail.com>
2019-12-21 01:14:33 +0530

    added go to page and total page count

--- js/script.js
+++ js/script.js
@@ -18,67 +18,63 @@ var current_page = 1;
 var records_per_page = 2;
 var selected = [];
 var objJson = [
-    { images: "assets/images/img1.jpeg"},
-    { images: "assets/images/img2.jpeg"},
-    { images: "assets/images/img3.jpeg"},
-    { images: "assets/images/img4.jpeg"},
-    { images: "assets/images/img5.jpeg"},
-    { images: "assets/images/img6.jpeg"},
-    { images: "assets/images/img7.jpeg"},
-    { images: "assets/images/img8.jpg"},
-    { images: "assets/images/img9.jpg"},
-    { images: "assets/images/img10.jpg"},
-    { images: "assets/images/img11.jpg"},
-    { images: "assets/images/img12.jpeg"},
-    { images: "assets/images/img13.jpg"},
-    { images: "assets/images/img14.jpg"},
-    { images: "assets/images/img15.jpg"},
-    { images: "assets/images/img16.jpg"},
-    { images: "assets/images/img17.jpg"},
-    { images: "assets/images/img18.jpg"},
-    { images: "assets/images/img19.jpg"},
-    { images: "assets/images/img20.jpg"}
+  { images: "assets/images/img1.jpeg" },
+  { images: "assets/images/img2.jpeg" },
+  { images: "assets/images/img3.jpeg" },
+  { images: "assets/images/img4.jpeg" },
+  { images: "assets/images/img5.jpeg" },
+  { images: "assets/images/img6.jpeg" },
+  { images: "assets/images/img7.jpeg" },
+  { images: "assets/images/img8.jpg" },
+  { images: "assets/images/img9.jpg" },
+  { images: "assets/images/img10.jpg" },
+  { images: "assets/images/img11.jpg" },
+  { images: "assets/images/img12.jpeg" },
+  { images: "assets/images/img13.jpg" },
+  { images: "assets/images/img14.jpg" },
+  { images: "assets/images/img15.jpg" },
+  { images: "assets/images/img16.jpg" },
+  { images: "assets/images/img17.jpg" },
+  { images: "assets/images/img18.jpg" },
+  { images: "assets/images/img19.jpg" },
+  { images: "assets/images/img20.jpg" }
 ]; // Can be obtained from another source, such as your objJson variable
 
-function prevPage()
-{
-    if (current_page > 1) {
-        current_page--;
-        changePage(current_page);
-        document.getElementById("result").innerHTML = "";
-    }
+function prevPage() {
+  if (current_page > 1) {
+    current_page--;
+    changePage(current_page);
+    document.getElementById("result").innerHTML = "";
+  }
 }
 
-function nextPage()
-{
-    if (current_page < numPages()) {
-      document.getElementById("result").innerHTML = "";
-        current_page++;
-        changePage(current_page);
+function nextPage() {
+  if (current_page < numPages()) {
+    document.getElementById("result").innerHTML = "";
+    current_page++;
+    changePage(current_page);
 
-    }
+  }
 }
 
-function changePage(page)
-{
-    var btn_next = document.getElementById("btn_next");
-    var btn_prev = document.getElementById("btn_prev");
-    var listing_table = document.getElementById("listingTable");
-    var page_span = document.getElementById("page");
-    console.log("slected",selected);
-    
-    // Validate page
-    if (page < 1) page = 1;
-    if (page > numPages()) page = numPages();
+function changePage(page) {
+  var btn_next = document.getElementById("btn_next");
+  var btn_prev = document.getElementById("btn_prev");
+  var listing_table = document.getElementById("listingTable");
+  var page_span = document.getElementById("page");
+  document.getElementById("totalPage").innerHTML = numPages();
+  // Validate page
+  if (page < 1) page = 1;
+  if (page > numPages()) page = numPages();
 
-    listing_table.innerHTML = "";
+  listing_table.innerHTML = "";
 
-    for (var i = (page-1) * records_per_page; i < (page * records_per_page); i++) {
-        // listing_table.innerHTML += objJson[i].adName + "<br>";
-        listing_table.innerHTML += "<img src="+ objJson[i].images+" width=\"400px\" height=\"200px\">" + "<br><br>";
+  for (var i = (page - 1) * records_per_page; i < (page * records_per_page); i++) {
+    // listing_table.innerHTML += objJson[i].adName + "<br>";
+    listing_table.innerHTML += "<img src=" + objJson[i].images + " width=\"400px\" height=\"200px\">" + "<br><br>";
 
-    }
-    listing_table.innerHTML +=`
+  }
+  listing_table.innerHTML += `
     <br><br>
     <form id="mainForm" name="mainForm">
       <table align="center">
@@ -98,31 +94,29 @@ function changePage(page)
       </table>
     </form>
     `
-    page_span.innerHTML = page;
-    var key = "page"+page;
-    if(selected.length > 0){
-      // console.log("kkk",selected[0]["page"+1]);
-      for (let i = 0; i < selected.length; i++) {
-        for(const keys in selected[i]){
-          if(keys === key){
-            document.getElementById("Radio"+selected[i][key]).checked = true;
-            console.log("kkk",selected[i][key]); 
-          }  
+  page_span.innerHTML = page;
+  var key = "page" + page;
+  if (selected.length > 0) {
+    for (let i = 0; i < selected.length; i++) {
+      for (const keys in selected[i]) {
+        if (keys === key) {
+          document.getElementById("Radio" + selected[i][key]).checked = true;
         }
       }
     }
-    
-    if (page == 1) {
-        btn_prev.style.visibility = "hidden";
-    } else {
-        btn_prev.style.visibility = "visible";
-    }
+  }
 
-    if (page == numPages()) {
-        btn_next.style.visibility = "hidden";
-    } else {
-        btn_next.style.visibility = "visible";
-    }
+  if (page == 1) {
+    btn_prev.style.visibility = "hidden";
+  } else {
+    btn_prev.style.visibility = "visible";
+  }
+
+  if (page == numPages()) {
+    btn_next.style.visibility = "hidden";
+  } else {
+    btn_next.style.visibility = "visible";
+  }
 }
 
 // document.mainForm.onclick = function(){
@@ -142,38 +136,54 @@ function changePage(page)
 
 function handleClick(myRadio) {
   var match = true;
-  document.getElementById('result').innerText = 'You selected: '+ myRadio.value;
+  document.getElementById('result').innerText = 'You selected: ' + myRadio.value;
   var pa = document.getElementById("page");
   var pageVal = pa.textContent;
   var obj = {};
-  var key = "page"+pageVal
+  var key = "page" + pageVal
   obj[key] = myRadio.value;
-  if(selected.length > 0){
+  if (selected.length > 0) {
     // console.log("kkk",selected[0]["page"+1]);
     for (let i = 0; i < selected.length; i++) {
-      for(const keys in selected[i]){
-        if(keys === key){
-          console.log("uuu");
+      for (const keys in selected[i]) {
+        if (keys === key) {
           match = false;
-          selected[i][key] = myRadio.value ;
-          console.log("kkk",selected[i][key]); 
-        }  
+          selected[i][key] = myRadio.value;
+        }
       }
     }
-    if(match){
+    if (match) {
       selected.push(obj);
     }
   }
-  else{
+  else {
     selected.push(obj);
   }
 }
 
-function numPages()
-{
-    return Math.ceil(objJson.length / records_per_page);
+
+function getPage() {
+  var input = document.getElementById("pageno");
+  if (input.value <= numPages()) {
+    changePage(input.value);
+  } else {
+    alert("Page NOT Found..!");
+  }
+
+}
+function numPages() {
+  return Math.ceil(objJson.length / records_per_page);
 }
 
-window.onload = function() {
-    changePage(1);
+window.onload = function () {
+  var input = document.getElementById("pageno");
+  input.addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
+      event.preventDefault();
+
+      document.getElementById("go").click();
+    }
+  });
+
+  changePage(1);
 };
